3f007ae544b348ff4afb996fa9f11bb3b334892d
Tyler Dahl <Tyler@pcp114596pcs.wireless.calpoly.edu>
2013-10-31 17:49:44 -0700

    Started an Xcode Project

---
+++ Scribble/Scribble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scribble
+//
+//  Created by Tyler Dahl on 10/31/13.
+//  Copyright (c) 2013 SketchySix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scribble/Scribble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scribble
+//
+//  Created by Tyler Dahl on 10/31/13.
+//  Copyright (c) 2013 SketchySix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
