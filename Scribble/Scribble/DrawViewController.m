d9eaf8bce7c6dc2f50f6c0e36c2dd4711bbbddee
Tyler Dahl <Tyler@pcp114756pcs.wireless.calpoly.edu>
2013-11-05 16:18:06 -0800

    Tyler added classes to keep track of different views

---
+++ Scribble/Scribble/DrawViewController.m
@@ -0,0 +1,38 @@
+//
+//  DrawViewController.m
+//  Scribble
+//
+//  Created by Tyler Dahl on 11/5/13.
+//  Copyright (c) 2013 SketchySix. All rights reserved.
+//
+
+#import "DrawViewController.h"
+
+@interface DrawViewController ()
+
+@end
+
+@implementation DrawViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Scribble/Scribble/FacebookViewController.m
@@ -0,0 +1,38 @@
+//
+//  FacebookViewController.m
+//  Scribble
+//
+//  Created by Tyler Dahl on 11/5/13.
+//  Copyright (c) 2013 SketchySix. All rights reserved.
+//
+
+#import "FacebookViewController.h"
+
+@interface FacebookViewController ()
+
+@end
+
+@implementation FacebookViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
