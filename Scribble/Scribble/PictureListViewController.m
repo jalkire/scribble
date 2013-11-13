ea8d322dd56e037b8b3f9a4929089df5238de586
John Alkire <jalkire@232x11.csc.calpoly.edu>
2013-11-12 17:42:54 -0800

    Working

--- Scribble/Scribble/PictureListViewController.m
+++ Scribble/Scribble/PictureListViewController.m
@@ -11,15 +11,15 @@
 
 @interface PictureListViewController ()
 
-@property NSArray *wallObjectsArray;
+@property (nonatomic, retain) NSArray *wallObjectsArray;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation PictureListViewController
 
-//@synthesize wallObjectsArray = _wallObjectsArray;
-//@synthesize scrollView = _scrollView;
+@synthesize wallObjectsArray = _wallObjectsArray;
+@synthesize scrollView = _scrollView;
 
 
 -(void)getPictures
@@ -65,11 +65,25 @@
     
     //go through array of photos and add each to scroll view
     for (PFObject *wallObject in self.wallObjectsArray){
-        UIView *PicturesListView = [[UIView alloc] initWithFrame:CGRectMake(10, originY, self.view.frame.size.width - 20 , 300)];
-        PFFile *pic = (PFFile *)[wallObject objectForKey:@"image"];
+        UIView *PicturesListView = [[UIView alloc] initWithFrame:CGRectMake(10, originY, self.view.frame.size.width , 300)];
+        
+        PFFile *pic = (PFFile *)[wallObject objectForKey:@"imageFile"];
         UIImageView *userPic = [[UIImageView alloc] initWithImage:[UIImage imageWithData:pic.getData]];
-        userPic.frame = CGRectMake(0, 0, PicturesListView.frame.size.width, 200);
+        userPic.frame = CGRectMake(0, 0, PicturesListView.frame.size.width, userPic.frame.size.height);
         [PicturesListView addSubview:userPic];
+        
+        
+        NSDate *creationDate = wallObject.createdAt;
+        NSDateFormatter *df = [[NSDateFormatter alloc] init];
+        [df setDateFormat:@"HH:mm dd/MM yyyy"];
+        //4
+        UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 210, PicturesListView.frame.size.width,15)];
+        infoLabel.text = [NSString stringWithFormat:@"Uploaded by: %@, %@", [wallObject objectForKey:@"user"], [df stringFromDate:creationDate]];
+        infoLabel.font = [UIFont fontWithName:@"Arial-ItalicMT" size:9];
+        infoLabel.textColor = [UIColor whiteColor];
+        infoLabel.backgroundColor = [UIColor clearColor];
+        [PicturesListView addSubview:infoLabel];
+        
         [self.scrollView addSubview:PicturesListView];
         
         //put gap between photos
